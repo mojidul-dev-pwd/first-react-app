1ede6abb50194f4b1aef1dc39cedd793cbd7272f
Mojidul Islam <mojidul.dev.pwd@gmail.com>
2024-02-27 13:48:01 +0600

    state-changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
+class CounterOld extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There is no tag!</p>;
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this)
+  // }
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "primary" : "warning";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
-export default Counter;
+export default CounterOld;
