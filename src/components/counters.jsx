d61bbfe32b11c44e41abccee77f977c794383bad
Mojidul <mojidul31@gmail.com>
2024-02-28 08:16:12 +0600

    handle-delete-2

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -23,8 +23,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
           >
             <h3>Counter # {counter.id}</h3>
           </Counter>
