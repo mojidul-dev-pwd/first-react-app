e13b041609fc9d1412435663bb1c0eedd30ffebc
Mojidul Islam <mojidul.dev.pwd@gmail.com>
2024-02-28 12:05:34 +0600

    add-nav-bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import "./App.css";
+import Counters from "./components/counters";
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>          
-        </header>
-        <p className="App-info">
-            Edit <code>src/App.js</code> and save to reload updated by Mojidul.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
